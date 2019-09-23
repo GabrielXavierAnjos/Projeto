bbee1eec5f4423b8df00a408af81f041b0a85436
GabrielXavierAnjos <47745596+GabrielXavierAnjos@users.noreply.github.com>
2019-09-23 11:24:53 -0300

    Add files via upload

---
+++ Projeto/Projeto/paginas/brasileiro.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  </script>
+  <title>Brasileiro</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand h1 mb-0" href="#">VIVA O MATE</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navebarSite">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navebarSite">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                      <a class="nav-link" href="../index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="brasileiro.php">Brasileiro</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="uruguaio.php">Uruguaio</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="curiosidades.php">Curiosidades</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="argentino.php">Argentino</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="paraguaio.php">Paraguaio</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="formulario.php">Cadastrar</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="navDrop">
+                      Socal
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">Facebook</a>
+                      <a class="dropdown-item" href="#">Twitter</a>
+                      <a class="dropdown-item" href="#">Instagram</a>
+                    </div>
+                  </li>
+                </ul>
+                <form class="form-inline">
+                  <input class="form-control ml-4 mr-2" type="search" placeholder="Buscar...">
+                  <button class="btn btn-dark" type="submit">Ok</button>
+                </form>
+              </div>
+      </div>
+  </nav>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center my-4">
+            <h1 class="display-3">Mate Brasileiro(Chimarrão)</h1>
+            <h4>A bebida com erva mate padrão brasileiro</h4>
+
+
+            <p>O chimarrão, ou mate, é uma bebida característica da cultura do sul da América do Sul legada pelas culturas indígenas caingangue, guarani, aimará e quíchua.
+             É composto por uma cuia, uma bomba, erva-mate moída e água a aproximadamente 70 graus Celsius.</p>
+          </div>
+        </div>
+        <!-- <div class="row">
+          <div class="col-3"></div>
+          <div class="col-9"></div>
+
+        </div> -->
+
+      </div>
+
+
+    <script src="js/jquery-3.4.1.js" ></script>
+    <script src="js/popper.min.js"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
+
+</body>
+
+</html>
